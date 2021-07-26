d73cef4b5c274e68dec509b3b0e120675624965f
omid shahidi <omid.shahidi@learninga-z.com>
2021-07-26 16:27:44 -0400

    Initial Commit

---
+++ Python Scripts/data_analyzer.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+
+class DataVisualizer:
+    def __init__(self):
+        # Windows Path issues: use string literal and fullpath to read using pandas
+        self.train = pd.read_json(
+            r'C:\Users\oshahidi\Desktop\Worlds Plate\Dataset\JSON\train.json')
+        self.train['number_ingredients'] = self.train['ingredients'].str.len()
---
+++ Python Scripts/preprocessing.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+
+class PreProcessor:
+    def __init__(self):
+        self.data = pd.read_json(
+            r'C:\Users\oshahidi\Desktop\Worlds Plate\Dataset\JSON\train.json')
+
+    def removeStopWords():
+        pass
+
+    def removeNumbers():
+        pass
+
+    def removePunctuation():
+        pass
