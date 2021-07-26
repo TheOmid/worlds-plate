4e01ab4d84902b0c5c4d060cae5e08dfcba83895
omid shahidi <omid.shahidi@learninga-z.com>
2021-07-26 16:46:13 -0400

    graph/get recipes per cuisine

--- Python Scripts/data_analyzer.py
+++ Python Scripts/data_analyzer.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import plotly.express as px
 
 
 class DataVisualizer:
@@ -7,3 +8,21 @@ class DataVisualizer:
         self.train = pd.read_json(
             r'C:\Users\oshahidi\Desktop\Worlds Plate\Dataset\JSON\train.json')
         self.train['number_ingredients'] = self.train['ingredients'].str.len()
+
+    def getRecipesPerCuisine(self):
+        recipiesPerCuisineCount = self.train['cuisine'].value_counts()
+        return pd.DataFrame({'cuisine': recipiesPerCuisineCount.index, 'number of recipes': recipiesPerCuisineCount.values})
+
+    def graphRecipesPerCuisine(self):
+        data = self.getRecipesPerCuisine()
+        fig = px.bar(data,
+                     x="number of recipes",
+                     y="cuisine", orientation='h',
+                     labels={
+                         "number of recipes": "Number of Recipes",
+                         "cuisine": "Cuisine",
+                         "species": "Species of Iris"
+                     },
+                     title="Number of Recipes Per Cuisine",
+                     color="cuisine")
+        fig.show()
